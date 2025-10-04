d7ff32aad36a10bbb490cfba25a3b36b360ef8d1
kbalint7 <196464789+kbalint7@users.noreply.github.com>
2025-10-04 21:03:58 +0200

    spaces instead of tabs

--- src/components/nav-button.tsx
+++ src/components/nav-button.tsx
@@ -4,21 +4,21 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 type Props = {
-	icon: LucideIcon;
-	label: string;
-	href?: string;
+    icon: LucideIcon;
+    label: string;
+    href?: string;
 };
 
 export function NavButton({ icon: Icon, label, href }: Props) {
-	return (
-		<Button variant="ghost" size="icon" aria-label={label} title={label} className="rounded-full" asChild>
-			{href ? (
-				<Link href={href}>
-					<Icon></Icon>
-				</Link>
-			) : (
-				<Icon />
-			)}
-		</Button>
-	);
+    return (
+        <Button variant="ghost" size="icon" aria-label={label} title={label} className="rounded-full" asChild>
+            {href ? (
+                <Link href={href}>
+                    <Icon></Icon>
+                </Link>
+            ) : (
+                <Icon />
+            )}
+        </Button>
+    );
 }
--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,24 +1,24 @@
 import { object, string, number } from 'zod';
 
 export const signInSchema = object({
-	email: string({ required_error: 'Email megadása kötelező' }).min(1, 'Email megadása kötelező').email('Érvénytelen email'),
-	password: string({ required_error: 'Jelszó megadása kötelező' })
-		.min(1, 'Jelszó megadása kötelező')
-		.min(8, 'A jelszónak legalább 8 karakter hosszúnak kell lennie')
-		.max(32, 'A jelszó legfeljebb 32 karakter hosszú lehet'),
+    email: string({ required_error: 'Email megadása kötelező' }).min(1, 'Email megadása kötelező').email('Érvénytelen email'),
+    password: string({ required_error: 'Jelszó megadása kötelező' })
+        .min(1, 'Jelszó megadása kötelező')
+        .min(8, 'A jelszónak legalább 8 karakter hosszúnak kell lennie')
+        .max(32, 'A jelszó legfeljebb 32 karakter hosszú lehet'),
 });
 
 export const signUpSchema = object({
-	firstName: string({ required_error: 'Keresztnév megadása kötelező' })
-		.min(1, 'Keresztnév megadása kötelező')
-		.max(255, 'A keresztnév legfeljebb 255 karakter hosszú lehet'),
-	lastName: string({ required_error: 'Vezetéknév megadása kötelező' })
-		.min(1, 'Vezetéknév megadása kötelező')
-		.max(255, 'A vezetéknév legfeljebb 255 karakter hosszú lehet'),
-	email: string({ required_error: 'Email megadása kötelező' }).min(1, 'Email megadása kötelező').email('Érvénytelen email'),
-	password: string({ required_error: 'Jelszó megadása kötelező' })
-		.min(1, 'Jelszó megadása kötelező')
-		.min(8, 'A jelszónak legalább 8 karakter hosszúnak kell lennie')
-		.max(32, 'A jelszó legfeljebb 32 karakter hosszú lehet'),
-	age: number({ required_error: 'Kor megadása kötelező' }).min(0, 'A kor nem lehet negatív'),
+    firstName: string({ required_error: 'Keresztnév megadása kötelező' })
+        .min(1, 'Keresztnév megadása kötelező')
+        .max(255, 'A keresztnév legfeljebb 255 karakter hosszú lehet'),
+    lastName: string({ required_error: 'Vezetéknév megadása kötelező' })
+        .min(1, 'Vezetéknév megadása kötelező')
+        .max(255, 'A vezetéknév legfeljebb 255 karakter hosszú lehet'),
+    email: string({ required_error: 'Email megadása kötelező' }).min(1, 'Email megadása kötelező').email('Érvénytelen email'),
+    password: string({ required_error: 'Jelszó megadása kötelező' })
+        .min(1, 'Jelszó megadása kötelező')
+        .min(8, 'A jelszónak legalább 8 karakter hosszúnak kell lennie')
+        .max(32, 'A jelszó legfeljebb 32 karakter hosszú lehet'),
+    age: number({ required_error: 'Kor megadása kötelező' }).min(0, 'A kor nem lehet negatív'),
 });
