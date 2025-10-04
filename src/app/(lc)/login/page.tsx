c3b9aa3f34d2f3f2a91987b5b15891756309507e
kbalint7 <196464789+kbalint7@users.noreply.github.com>
2025-10-04 20:52:12 +0200

    login, register: redirect to dashboard when signed in

--- src/app/(lc)/login/page.tsx
+++ src/app/(lc)/login/page.tsx
@@ -1,6 +1,17 @@
 import { LoginForm } from '@/components/login-form';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (session) {
+        redirect('/dashboard');
+    }
+
     return (
         <main className="flex flex-col items-center p-4">
             <LoginForm />
--- src/app/(lc)/register/page.tsx
+++ src/app/(lc)/register/page.tsx
@@ -1,6 +1,17 @@
 import { RegisterForm } from '@/components/register-form';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (session) {
+        redirect('/dashboard');
+    }
+
     return (
         <main className="flex flex-col items-center p-4">
             <RegisterForm />
